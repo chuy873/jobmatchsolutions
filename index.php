2e3e3e41b70b8708fb3d5e070e3f6d0a949bfe31
chuy873 <chuy873@gmail.com>
2024-10-22 00:15:48 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página en Construcción</title>
+</head>
+<body>
+    <h1>Página en Construcción</h1>
+    <p>Estamos trabajando en algo increíble. ¡Vuelve pronto!</p>
+</body>
+</html>
